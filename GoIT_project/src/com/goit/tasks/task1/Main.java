3d91936cd276f21fa35dd120a83fc5e1345c3534
Aset Uisin <jobs.uisin@gmail.com>
2015-06-10 00:29:11 +0600

    6 comments from Dmitry

--- GoIT_project/src/com/goit/tasks/task1/Main.java
+++ GoIT_project/src/com/goit/tasks/task1/Main.java
@@ -10,10 +10,11 @@ public class Main {
     public static void main(String[] args) {
     	String wellcom = "Please enter your text";
     	System.out.println(wellcom);
+    	
     	Scanner scanner = new Scanner(System.in);
         String text = scanner.nextLine();
        
-        AnagramAction myAnagram = new AnagramAction();
+        AnagramActor myAnagram = new AnagramActor();
         String anagram = myAnagram.getAnagram(text);
         
         System.out.println(anagram);
